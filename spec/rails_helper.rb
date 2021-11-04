fe6c053757abd967e3910a8fdf0af607892318af
Gunnar <dunkcrunk.com>
2021-11-03 23:37:34 -0600

    created my schema and placed in the readme.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -54,6 +54,7 @@ VCR.configure do |config|
 end
 
 RSpec.configure do |config|
+  config.include FactoryBot::Syntax::Methods
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
